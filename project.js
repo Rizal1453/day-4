a30bba1c32d72bf04f1af92852c1f6af8e1878c1
Rizal Mantopani <mntpnijal@gmail.com>
2022-09-16 00:29:20 +0700

    first commit

--- project.js
+++ project.js
@@ -2,11 +2,15 @@ let data =[]
 
 function addData (event){
     event.preventDefault();
-        let projectName =document.getElementById("input-project").value
-        let startDate   =document.getElementById("input-start").value
-        let endDate     =document.getElementById("input-end").value
-        let description =document.getElementById("input-description").value
-        let image       =document.getElementById("input-image").files
+        let projectName = document.getElementById("input-project").value
+        let startDate = document.getElementById("input-start").value
+        let endDate = document.getElementById("input-end").value
+        let nodeJs = document.getElementById("nodeJs").checked
+        let nextJs = document.getElementById("nextJs").checked
+        let reactJs = document.getElementById("reactJs").checked
+        let typescript = document.getElementById("typescript").checked
+
+        let image = document.getElementById("input-image").files
         image = URL.createObjectURL(image[0])
    
        
@@ -16,7 +20,11 @@ function addData (event){
         startDate,
         endDate,
         description,
-        image
+        image,
+        nodeJs,
+        nextJs,
+        reactJs,
+        typescript
         
     }
     data.push(tampung);
@@ -34,23 +42,20 @@ function addData (event){
 
 
            document.getElementById("card1").innerHTML +=`
-           <div>
-              <img src="${data[index].image}" alt="">
-            <div>
-                <h3>${data[index].projectName}</h3>
-                <p class="time">durasi : 3 bulan</p>
-                <br>
-                <p>${data[index].description}<br>
-                    Happy download</p>
-                <div>
-                    <i class="fa-brands fa-google-play"></i>
-                    <i class="fa-brands fa-apple"></i>
-                    <i class="fa-brands fa-java"></i>
-                </div>
-                <div class="action">
-                    <button class="edit">edit</button>
-                    <button class="delete">delete</button>
-                </div>
+           <div class="card" style="
+           margin: 20px auto;
+           width: 250px;
+           text-align: center;">
+            <img src="${data[index].image}" alt="" width="100%">
+            <p>${data[index].startDate}</p>
+            <div class="content">
+            ${data[index.description]}
+            <div class="action">
+            <button class="edit">edit</button>
+            <button class="delete">delete</button>
+            </div>
+
+            </div>
             </div>
                 `
 
